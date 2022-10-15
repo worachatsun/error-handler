9240fa911250c385da100b97cc6ed33841da3af0
worachatsun <worachat.sun@gmail.com>
2022-10-16 01:19:33 +0700

    Add test wrap function

--- src/__tests__/custom-error.test.ts
+++ src/__tests__/custom-error.test.ts
@@ -20,4 +20,49 @@ describe('CustomError', () => {
       expect(customError.trace).toHaveLength(0)
     })
   })
+
+  describe('wrap', () => {
+    let customError: CustomError
+
+    beforeEach(() => {
+      customError = new CustomError(error)
+    })
+
+    test('error tracing should be increase when get wrap', () => {
+      const trace = { source: 'Controller' }
+      customError.wrap(trace)
+      expect(customError.error).toEqual(error)
+      expect(customError.apiCode).not.toBeDefined()
+      expect(customError.trace).toHaveLength(1)
+    })
+
+    test('error tracing should be increase when get wrap', () => {
+      const trace = { source: 'Controller', metadata: { id: 'test-id' } }
+      customError.wrap(trace)
+      expect(customError.error).toEqual(error)
+      expect(customError.apiCode).not.toBeDefined()
+      expect(customError.trace).toHaveLength(1)
+      expect(customError.trace).toEqual([trace])
+    })
+
+    test('error tracing should be increase when get wrap (error with api code)', () => {
+      const apiCode = 'XX-X-000'
+      const customError = new CustomError(error, apiCode)
+      const trace = { source: 'Controller' }
+      customError.wrap(trace)
+      expect(customError.error).toEqual(error)
+      expect(customError.trace).toHaveLength(1)
+    })
+
+    test('error tracing should be increase to two when wrap twice', () => {
+      const traceController = { source: 'Controller', metadata: { id: 'test-id' } }
+      const traceService = { source: 'Service' }
+      customError.wrap(traceController)
+      customError.wrap(traceService)
+      expect(customError.error).toEqual(error)
+      expect(customError.apiCode).not.toBeDefined()
+      expect(customError.trace).toHaveLength(2)
+      expect(customError.trace).toEqual([traceController, traceService])
+    })
+  })
 })
