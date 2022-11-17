31c09ef03a200386f356b770b05cc0582f5120ce
Worachat Arunothaikrit <worachat@rentspree.com>
2022-11-17 19:26:32 +0700

    Add rollup package and publish config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import external from 'rollup-plugin-peer-deps-external'
+import packageJson from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: 'index'
+    }
+  ],
+  plugins: [external(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+}
